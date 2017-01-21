26ac9f22590c17c7c369a4d2d3306a98e2108019
Dan Scott <dj-scott1@hotmail.co.uk>
2017-01-21 18:01:50 +0000

    Created php file to retrieve usernames and email from database

---
+++ WAD-Assignment/get-uname-email.php
@@ -0,0 +1,14 @@
+<?php
+  require ('includes/conn.inc.php');
+
+  $stmt = $pdo->prepare("SELECT Username, Email FROM User");
+  $stmt->execute();
+  $row = $stmt->fetchObject();
+
+  $returnArr = array();
+  while($row = $stmt->fetch()){
+    $returnArr[] = array("Username" => $row['Username'], "Email" => row['Email'])
+  }
+
+  echo json_encode($returnArr);
+?>
