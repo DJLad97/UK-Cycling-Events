4787d077084ed550d7523be7ea8f877c7edc0df5
Dan Scott <dj-scott1@hotmail.co.uk>
2017-01-21 18:22:30 +0000

    Changed to more appropriate name

---
+++ WAD-Assignment/get-race-details.php
@@ -0,0 +1,19 @@
+<?php
+  // This code is not retriveing the first row from the database
+  //header('Content-Type: applications/json');
+  require('includes/conn.inc.php');
+
+  $stmt = $pdo->prepare("SELECT RaceID, RaceType, RaceName, RaceLatLong, RaceDate FROM races");
+  $stmt->execute();
+  $row = $stmt->fetchObject();
+
+  //echo json_encode($row)
+ $returnArr = array();
+ while($row = $stmt->fetch()){
+
+   $returnArr[] = array("RaceID" => $row['RaceID'], "RaceType" => $row['RaceType'], "RaceName" => $row['RaceName'],
+                        "RaceLatLong" => $row['RaceLatLong'], "RaceDate" => $row['RaceDate']);
+ }
+
+  echo json_encode($returnArr);
+?>
