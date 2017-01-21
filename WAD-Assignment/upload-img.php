817a156277f457a0689ed303b9a16d23e47da205
Dan Scott <dj-scott1@hotmail.co.uk>
2017-01-21 21:39:00 +0000

    Added the image upload code and it works fine (just not on the html side)

--- WAD-Assignment/upload-img.php
+++ WAD-Assignment/upload-img.php
@@ -1,11 +1,52 @@
 <?php
-  if(isset($_FILES['file'])){
-    $allowed = array('jpg', 'png');
+  require('includes/conn.inc.php');
 
-    if(in_array($fileExt, $allowed)) {
-      if($fileError === 0){
+  $userID = $_SESSION['userSession'];
 
-      }
-    }
+  $fileError = $_FILES['profileImg']['error'];
+  // http://php.net/manual/en/features.file-upload.errors.php
+  if($fileError > 0){
+      header("Location:profile.php?err=imgUploadError");
+      exit;
   }
+
+  
+
+  $fileTempName = $_FILES['profileImg']['tmp_name'];
+  $trueFileType = exif_imagetype($fileTempName);
+  $allowedFiles = array(IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG);
+
+  if (in_array($trueFileType, $allowedFiles)) {
+    switch($trueFileType){
+      case 1: $fileExt  = ".gif";
+        break;
+      case 2: $fileExt  = ".jpg";
+        break;
+      case 3 : $fileExt  = ".png";
+        break;
+     }
+}
+  else{
+    header("Location:profile.php?err=WrongFileType");
+    exit;
+  }
+
+  $newFileName = $userID . $fileExt;
+
+  $myPathInfo = pathinfo($_SERVER['DOCUMENT_ROOT'].$_SERVER['PHP_SELF']);
+  $currentDir = $myPathInfo['dirname'];
+  $imgDir = $currentDir . '/images/';
+
+
+
+  $stmt = $pdo->prepare("UPDATE user SET profileImg = :profileImg WHERE UserID = :userID");
+  $stmt->bindParam(":profileImg", $newFileName);
+  $stmt->bindParam(":userID", $userID, PDO::PARAM_INT);
+  $stmt->execute();
+
+  $newImgLocation = $imgDir . $newFileName;
+  if(move_uploaded_file($fileTempName, $newImgLocation))
+    header("Location: profile.php");
+  else
+    header("Location:profile.php?err=uploadProb");
 ?>
