f454b98da275b910a965add203d4a076e4349cfe
Dan Scott <dj-scott1@hotmail.co.uk>
2017-01-21 19:41:09 +0000

    Now gets first row in DB

--- WAD-Assignment/get-uname-email.php
+++ WAD-Assignment/get-uname-email.php
@@ -1,11 +1,10 @@
 <?php
+
   require ('includes/conn.inc.php');
 
   $stmt = $pdo->prepare("SELECT Username, Email FROM User");
   $stmt->execute();
-  $row = $stmt->fetchObject();
 
-  $returnArr = array();
   while($row = $stmt->fetch()){
     $returnArr[] = array("Username" => $row['Username'], "Email" => $row['Email']);
   }
