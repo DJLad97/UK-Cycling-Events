732e7d205e6be015a5e30b409615c6750b7a622a
Dan Scott <dj-scott1@hotmail.co.uk>
2017-01-23 21:22:17 +0000

    Created the activate account script

---
+++ WAD-Assignment/activate.php
@@ -0,0 +1,19 @@
+<?php
+  require('includes/conn.inc.php');
+  $userID = trim($_GET['x']);
+  $active = trim($_GET['y']);
+
+  if(is_numeric($userID) && !empty($active)){
+    $stmt  = $pdo->prepare("UPDATE user SET active = 'Yes' WHERE userID = :userID AND active = :active");
+    $stmt->bindParam(':userID', $userID);
+    $stmt->bindParam(':active', $active);
+
+    if($stmt->rowCount == 1){
+      header('Location: login.php?action=active');
+      exit;
+    }
+    else {
+      echo "Your account could not be activated";
+    }
+  }
+?>
