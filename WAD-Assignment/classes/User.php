e90c5ca2a2f178fa90a5ee71638a9d224c3028dd
Dan Scott <dj-scott1@hotmail.co.uk>
2017-01-23 21:25:42 +0000

    Can now send an email to the email they signed up with

--- WAD-Assignment/classes/User.php
+++ WAD-Assignment/classes/User.php
@@ -1,4 +1,6 @@
 <?php
+define('DIR', 'http://localhost:1234/UK%20Cycling%20Events/WAD-Assignment/');
+define('SITEEMAIL', 'noreply@UK-Cycling-Events.co.uk');
 class user
 {
   private $db;
@@ -20,13 +22,14 @@ class user
     try
     {
       // $newPass = $password_hash($password, PASSWORD_DEFAULT);
+      $activation = md5(uniqid(rand(), true));
       $safePass = filter_var($password, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
       $hashedPass = password_hash($safePass, PASSWORD_BCRYPT);
       $this->hashedPassGlb = $hashedPass;
       $stmt = $this->db->prepare("INSERT INTO User(Forename, Surname, Username, Email, Password,
-      AddressLine1, AddressLine2, TownCity, County, PostCode, Country)
+      AddressLine1, AddressLine2, TownCity, County, PostCode, Country, active)
       VALUES(:fName, :sName, :uName, :email, :password, :add1, :add2,
-              :townCity, :county, :postCode, :country)");
+              :townCity, :county, :postCode, :country, :active)");
 
       $stmt->bindParam(':fName', $fName);
       $stmt->bindParam(':sName', $sName);
@@ -39,8 +42,28 @@ class user
       $stmt->bindParam(':county', $county);
       $stmt->bindParam(':postCode', $postCode);
       $stmt->bindParam(':country', $country);
+      $stmt->bindParam(':active', $activation);
 
       $stmt->execute();
+
+      $userID = $this->db->lastInsertId('UserID');
+
+      $to = $email;
+      $subject = "Registration Confirmation";
+      $body = "<p>Thank you for registering for the site!</p>
+      <p>To activate your account, please click on this link: <a href='".DIR."activate.php?x=$userID&y=$activation'>".DIR."activate.php?x=$userID&y=$activation</a></p>
+			<p>Regards Site Admin</p>";
+
+      $header = "MIME-Version: 1.0\r\n";
+      $header .= "Content-type: text/html\r\n";
+
+
+      if(mail($to,$subject,$body,$header)){
+         echo "Message sent successfully...";
+      }
+      else {
+         echo "Message could not be sent...";
+      }
 
       return $stmt;
     }
