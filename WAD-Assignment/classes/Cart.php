e6755b0a3a3b1795f5c3b2ac604c13f4b72f2e6a
Dan Scott <dj-scott1@hotmail.co.uk>
2017-01-27 18:34:06 +0000

    Add a proper addItem function

--- WAD-Assignment/classes/Cart.php
+++ WAD-Assignment/classes/Cart.php
@@ -4,19 +4,23 @@ class Cart
   public $noOfItems;
   public $cartTotal;
   public $cartArr = array();
-  // public $cartArr = array('RaceID' => array(), 'Name' => array(), 'Price' => array());
 
   public function __construct(){
     $this->noOfItems = 0;
     $this->cartTotal = 0;
   }
 
-  public function addItem($raceID, $name, $price){
-    array_push($this->cartArr, array($raceID, $name, $price));
+  public function addItem($raceID, $raceName, $price){
+    array_push($this->cartArr, array("RaceID" => $raceID, "RaceName" => $raceName, "EntryPrice" => $price));
+    $this->noOfItems++;
   }
 
   public function showCart(){
-    return $this->cartArr;
+    //return $this->cartArr;
+
+    foreach ($this->cartArr as $var) {
+      echo "\n", $var['RaceID'], "\t\t", $var['RaceName'], "\t\t", $var["EntryPrice"];
+    }
   }
 }
 ?>
