b47b1e4bb74deafbdc56b999cef8ffc343f6ee9d
Dan Scott <dj-scott1@hotmail.co.uk>
2017-01-30 20:56:11 +0000

    Changed add to cart to accommodate gender and age range values

--- WAD-Assignment/add-to-cart.php
+++ WAD-Assignment/add-to-cart.php
@@ -3,16 +3,15 @@ require('includes/conn.inc.php');
 $raceID = $_GET['RaceID'];
 $raceName = $_GET['RaceName'];
 $price = $_GET['EntryPrice'];
-
-if($price == NULL)
-  header('Location: insert-race-sign-up.php');
+$gender = $_GET['gender'];
+$ageRange = $_GET['ageRange'];
 
 if(!isset($_SESSION['cart'])){
   $cart = new Cart();
   $_SESSION['cart'] = $cart;
 }
 
-$_SESSION['cart']->addItem($raceID, $raceName, $price);
-header('Location: index.php');
+$_SESSION['cart']->addItem($raceID, $raceName, $price, $gender, $ageRange);
+header('Location: race-cart.php');
 exit;
 ?>
