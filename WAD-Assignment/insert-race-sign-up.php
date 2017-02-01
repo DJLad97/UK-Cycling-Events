5e6bd5282c3bdc29997abf5dfbfa2aeb08910cdf
Dan Scott <dj-scott1@hotmail.co.uk>
2017-02-01 16:57:46 +0000

    Changed what gets inserted into the DB based on if was the entry to race was free or not

--- WAD-Assignment/insert-race-sign-up.php
+++ WAD-Assignment/insert-race-sign-up.php
@@ -1,7 +1,6 @@
 <?php
   require('includes/conn.inc.php');
   require_once('includes/functions.inc.php');
-
   $userID = $_SESSION['userSession'];
 
   $userQuery = "SELECT UserID, Forename, Surname FROM user WHERE UserID = :userID";
@@ -16,25 +15,30 @@
   $raceID = $_SESSION['getRaceID'];
   $userID = $userRow['UserID'];
   $name = $nameFromUser;
-  $gender = $_POST['gender'];
-  $ageRange = $_POST['ageRange'];
+  
+  if(!isset($_POST['checkout'])){
+    $gender = $_POST['gender'];
+    $ageRange = $_POST['ageRange'];
+  }
 
-  echo "RaceID" . $raceID;
-  echo "UserID" . $userID;
-  echo "Name" . $name;
-  echo "Gender" . $gender;
-  echo "AgeRange" . $ageRange;
+  if(isset($_POST['checkout'])){
+    $sql = $_POST['sqlQuery'];
+  }
+  else {
+    $sql = "INSERT INTO RaceSignUp (RaceID, UserID, Name, Gender, AgeRange)
+            VALUES (:raceID, :userID, :name, :gender, :ageRange)";
+  }
 
-  $sql = "INSERT INTO RaceSignUp (RaceID, UserID, Name, Gender, AgeRange)
-          VALUES (:raceID, :userID, :name, :gender, :ageRange)";
-
-
+  //echo $sql;
   $stmt = $pdo->prepare($sql);
-  $stmt->bindParam(":raceID", $raceID, PDO::PARAM_INT);
-  $stmt->bindParam(":userID", $userID, PDO::PARAM_INT);
-  $stmt->bindParam(":name", $name, PDO::PARAM_STR);
-  $stmt->bindParam(":gender", $gender, PDO::PARAM_STR);
-  $stmt->bindParam(":ageRange", $ageRange, PDO::PARAM_STR);
+  if(!isset($_POST['checkout'])){
+    $stmt->bindParam(":raceID", $raceID, PDO::PARAM_INT);
+    $stmt->bindParam(":userID", $userID, PDO::PARAM_INT);
+    $stmt->bindParam(":name", $name, PDO::PARAM_STR);
+    $stmt->bindParam(":gender", $gender, PDO::PARAM_STR);
+    $stmt->bindParam(":ageRange", $ageRange, PDO::PARAM_STR);
+  }
+
   $stmt->execute();
 
   unset($_SESSION['cart']);
