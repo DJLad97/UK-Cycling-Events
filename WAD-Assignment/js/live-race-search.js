ac1ec76fcec68cb7c47cd75a3dd4e07866f7d0cc
Dan Scott <dj-scott1@hotmail.co.uk>
2017-02-02 21:15:25 +0000

    Commit this

--- WAD-Assignment/js/live-race-search.js
+++ WAD-Assignment/js/live-race-search.js
@@ -1,18 +1,45 @@
 $(document).ready(function() {
+    var previewHTML = '';
     $('#searchTerm').on('input', function(){
       var searchKeyword = $(this).val();
 
-      if(searchKeyword.length >= 3){
+      if(searchKeyword.length >= 1){
         $.post(
           'search-race.php',
           {searchTerm: searchKeyword},
           function(data){
             $('ul#content').empty();
             $.each(data, function(){
+              previewHTML = '<div class="parent"><div class="preview"><a href="#" data-id=' +
+                                  this.RaceID + ' class="getPreview">Preview</a></div>' +
+                                  '<div class="fullDetails"></div></div>';
+
               $('ul#content').append('<li><a href="race-sign-up.php?RaceID=' +
-                                    this.RaceID + '">' + this.RaceName + '</a></li>');
+                                    this.RaceID + '">' + this.RaceName + '</a>' + previewHTML + '</li>');
+
+              // $('ul#content').append('<li><a href="race-sign-up.php?RaceID=' +
+              //                       this.RaceID + '">' + this.RaceName + '</a>' +
+              //                       '<div class="preview"><a href="#" data-id=' +
+              //                       this.RaceID + ' class="getPreview">Preview</a></div></li>');
             })
           }, "json");
       }
     });
+    $(document).on('click', '.getPreview', function(ev){
+      ev.preventDefault();
+      var myVars = {'RaceID': $(this).attr('data-id')};
+      var currentNode = $(this);
+      var detailsNode = $(this).parents('.parent').find('.fullDetails')
+      $.get('search-race.php', myVars, function(data){
+        alert("In get");
+        // $(detailsNode).html(data.RaceDate + ' - ' +
+        // data.ClosingEntryDate + ' - ' +
+        // data.RaceType).slideToggle(500);
+        console.dir(data);
+        $('.fullDetails').html(data.RaceDate + ' - ' +
+        data.ClosingEntryDate + ' - ' +
+        data.RaceType);
+      }, 'json');
+    });
+
 });
