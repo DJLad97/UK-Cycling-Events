57424255c911b8432957a893d8807eef11f7a313
Dan Scott <dj-scott1@hotmail.co.uk>
2017-02-02 21:15:04 +0000

    Created php navbar header file so I just need include where I want (Might use this when I go back to the main site)

---
+++ WAD-Assignment/CMS/header.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">CMS</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="CMS.php">Home</a></li>
+    </ul>
+  </div>
+</nav>
