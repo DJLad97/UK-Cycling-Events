707f115645e93c5b6f421668b67e972cd89769a1
Dan Scott (SP4) <dj-scott1@hotmail.co.uk>
2017-02-04 19:18:52 +0000

    Added the username to comment table

--- WAD-Assignment/add-comment.php
+++ WAD-Assignment/add-comment.php
@@ -6,9 +6,15 @@ $userID = $_SESSION['userSession'];
 
 $commentContent = filter_var(strip_tags(trim($_POST['raceComment'])), FILTER_SANITIZE_STRING);
 
-$sql = "INSERT INTO comment (CommentContent, UserID) VALUES (:commentContent, :userID)";
+$userSQL = "SELECT Username FROM user WHERE UserID = :userID";
+$userStmt = $pdo->prepare($userSQL);
+$userStmt->bindParam(':userID', $userID, PDO::PARAM_INT);
+$userStmt->execute();
+
+$sql = "INSERT INTO comment (CommentContent, UserID, Username) VALUES (:commentContent, :userID, :userName)";
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(':commentContent', $commentContent, PDO::PARAM_STR);
+$stmt->bindParam(':userName', $userStmt->Username, PDO::PARAM_STR);
 $stmt->bindParam(':userID', $userID, PDO::PARAM_INT);
 $stmt->execute();
 header('Location: race-sign-up.php?RaceID=' . $raceID);
