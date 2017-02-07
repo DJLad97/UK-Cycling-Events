e685ed73333ebd868d3c06105cad94928e82b540
Dan Scott <dj-scott1@hotmail.co.uk>
2017-02-07 18:50:17 +0000

    Created remove from cart script

---
+++ WAD-Assignment/remove-from-cart.php
@@ -0,0 +1,22 @@
+<?php
+require('includes/conn.inc.php');
+ $itemToRemove = $_POST['cart-item'];
+
+ // Create temp array so I can reset the index the elements are in
+ // E.g array[0], array[1], array[2], array[3]
+ // I remove array[1] the elements will reset so I will now have
+ // array[0], array[1], array[2]
+ $tempArr = $_SESSION['cart']->cartArr;
+
+for($i = 0; $i < count($tempArr); $i++){
+  // Remove the item the user requested
+  if($tempArr[$i]['RaceID'] == $itemToRemove){
+    unset($tempArr[$i]);
+  }
+}
+
+// Do what I mentioned above
+$_SESSION['cart']->cartArr = array_values($tempArr);
+
+header('Location: race-cart.php');
+?>
